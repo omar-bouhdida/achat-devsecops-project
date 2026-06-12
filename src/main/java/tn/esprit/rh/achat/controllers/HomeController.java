7851ebb62b5ab28cb6c4963459fd984a21e6a029
omar-bouhdida <omar.bouhdida@sesame.com.tn>
2026-06-12 13:15:36 +0100

    Fix Java 8 compatibility

--- src/main/java/tn/esprit/rh/achat/controllers/HomeController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/HomeController.java
@@ -5,17 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
     @GetMapping("/")
-    public String home() {
-        return """
-            <html>
-            <body style="font-family: Arial; text-align:center; margin-top:100px;">
-                <h1>🚀 Application Running</h1>
-                <h2>Spring Boot + Docker + MySQL</h2>
-                <p>The application is successfully deployed in Docker.</p>
-            </body>
-            </html>
-            """;
-    }
+        public String home() {
+         return "Application Running";
+        }
 }
